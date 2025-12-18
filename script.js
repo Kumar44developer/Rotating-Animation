0025a3f42d7d01244a7bb4eadd960647d1a681a3
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-12-18 15:36:03 +0530

    Calculated rotation degree based on square index

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ const squares = document.querySelectorAll(".squares");
 squares.forEach((square,index)=>{
 square.style.borderWidth = (index + 1) * 4 + "px";
         square.style.animationName = `rotate-${index}`;
+     const degree = (index + 1) * 360 *2;
