828d7cbb0bed7d9ed9f3d233eb53a946f373de7a
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-12-18 15:37:06 +0530

    Defined dynamic keyframes for square rotation

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ square.style.borderWidth = (index + 1) * 4 + "px";
         square.style.animationName = `rotate-${index}`;
          const degree = (index + 1) * 360 *2;
         const animation = document.createElement("style");
+        animation.innerHTML = `@keyframes rotate-${index}
