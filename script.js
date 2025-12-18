428b1430b1738ee1dd14c934a630ead5b9439fbc
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-12-18 15:36:35 +0530

    Created style element for dynamic animations

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ const squares = document.querySelectorAll(".squares");
 squares.forEach((square,index)=>{
 square.style.borderWidth = (index + 1) * 4 + "px";
         square.style.animationName = `rotate-${index}`;
-     const degree = (index + 1) * 360 *2;
+         const degree = (index + 1) * 360 *2;
+        const animation = document.createElement("style");
